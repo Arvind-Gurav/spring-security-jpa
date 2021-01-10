d326422b179f69ddb0ea613ae1a683469ce64387
Arvind-Gurav <arvindgurav08@gmail.com>
2021-01-11 00:12:48 +0530

    security

--- src/main/java/io/javabrains/springsecurityjpa/webService/HomeResource.java
+++ src/main/java/io/javabrains/springsecurityjpa/webService/HomeResource.java
@@ -1,4 +1,4 @@
-package io.javabrains.springsecurityjpa;
+package io.javabrains.springsecurityjpa.webService;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
